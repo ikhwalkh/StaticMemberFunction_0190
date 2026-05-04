5358be6b710dc9cc39db4bd8e48b16882f118ed7
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-04 15:48:46 +0700

    mahasiswa

--- constructorverloading.cpp
+++ constructorverloading.cpp
@@ -6,5 +6,27 @@ class mahasiswa{
     private:
     int nim;
     string nama;
-    
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa :: mahasiswa(){
 }
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+

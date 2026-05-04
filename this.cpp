57844447117883eca400d784c1d3871d3448bced
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-05-04 16:34:31 +0700

    // return variabel judul.

--- this.cpp
+++ this.cpp
@@ -8,7 +8,7 @@ class buku{
     public:
     string setget(string judul){
         this->judul= judul;    //nilai parameter 'judul' untuk member variabel 'judul'
-        return this->judul;
+        return this->judul;   // return variabel judul.
     }
 };
 
